58233d10388feb23219cf2da3047196295d3545a
Joao Rocha <jrocha@silicolife.com>
2026-02-26 23:30:52 +0000

    Added GrenadeRenderer!

---
+++ client/src/GrenadeRenderer.tsx
@@ -0,0 +1,45 @@
+import { Connection } from "./Connection";
+import { DetonatePosition } from "./DetonatePosition";
+import { ThrowPosition } from "./ThrowPosition";
+
+interface Props {
+  throwPosition: { x: number; y: number };
+  detonatePosition: { x: number; y: number };
+  trajectory?: { x: number; y: number }[];
+}
+
+export function GrenadeRenderer({
+  throwPosition,
+  detonatePosition,
+  trajectory = [],
+}: Props) {
+  return (
+    <>
+      {trajectory.length > 0 ? (
+        <>
+          <Connection from={throwPosition} to={trajectory[0]} />
+          {trajectory.map((point, index) => {
+            const nextPoint = trajectory[index + 1] || detonatePosition;
+
+            return (
+              <Connection
+                key={`segment-${index}`}
+                from={point}
+                to={nextPoint}
+              />
+            );
+          })}
+          <Connection
+            from={trajectory[trajectory.length - 1]}
+            to={detonatePosition}
+          />
+        </>
+      ) : (
+        <Connection from={throwPosition} to={detonatePosition} />
+      )}
+
+      <ThrowPosition x={throwPosition.x} y={throwPosition.y} />
+      <DetonatePosition x={detonatePosition.x} y={detonatePosition.y} />
+    </>
+  );
+}
--- client/src/Minimap.tsx
+++ client/src/Minimap.tsx
@@ -1,6 +1,4 @@
-import { Connection } from "./Connection";
-import { DetonatePosition } from "./DetonatePosition";
-import { ThrowPosition } from "./ThrowPosition";
+import { GrenadeRenderer } from "./GrenadeRenderer";
 
 export function Minimap() {
   return (
@@ -8,23 +6,16 @@ export function Minimap() {
       <svg viewBox="0 0 1024 1024" className="h-full w-full">
         <image href="/de_mirage_filtered.png" width="1024" height="1024" />
 
-        <Connection
-          from={{ x: 899.46875, y: -1040.25 }}
-          to={{ x: -797.5625, y: -1596.71875 }}
+        <GrenadeRenderer
+          throwPosition={{ x: 899.46875, y: -1040.25 }}
+          detonatePosition={{ x: -635.375, y: -1649.90625 }}
+          trajectory={[{ x: -797.5625, y: -1596.71875 }]}
         />
-        <Connection
-          from={{ x: -797.5625, y: -1596.71875 }}
-          to={{ x: -635.375, y: -1649.90625 }}
-        />
-        <ThrowPosition x={899.46875} y={-1040.25} />
-        <DetonatePosition x={-635.375} y={-1649.90625} />
 
-        <Connection
-          from={{ x: 1404.75, y: 64.9375 }}
-          to={{ x: -236.1875, y: -496.46875 }}
+        <GrenadeRenderer
+          throwPosition={{ x: 1404.75, y: 64.9375 }}
+          detonatePosition={{ x: -236.1875, y: -496.46875 }}
         />
-        <ThrowPosition x={1404.75} y={64.9375} />
-        <DetonatePosition x={-236.1875} y={-496.46875} />
       </svg>
     </div>
   );
