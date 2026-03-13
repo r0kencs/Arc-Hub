b32fdfc09a5fa63506947b096e7ea28ba68b1d7f
Joao Rocha <jrocha@silicolife.com>
2026-03-13 01:12:30 +0000

    Added Table!

--- client/src/routes/spawns.tsx
+++ client/src/routes/spawns.tsx
@@ -1,9 +1,186 @@
+import { Button } from "@/components/ui/button";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Delete, Delete01Icon } from "@hugeicons/core-free-icons";
+import { HugeiconsIcon } from "@hugeicons/react";
 import { createFileRoute } from "@tanstack/react-router";
+import {
+  type ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  getPaginationRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
 
 export const Route = createFileRoute("/spawns")({
   component: RouteComponent,
 });
 
+type Spawn = {
+  id: string;
+  name: string;
+
+  x: number;
+  y: number;
+  z: number;
+
+  pitch: number;
+  yaw: number;
+};
+
+const spawns: Spawn[] = [
+  {
+    id: "audhwhuad",
+    name: "Spawn 1",
+    x: 0,
+    y: 0,
+    z: 0,
+    pitch: 0,
+    yaw: 0,
+  },
+  {
+    id: "djadadjw",
+    name: "Spawn 2",
+    x: 0,
+    y: 0,
+    z: 0,
+    pitch: 0,
+    yaw: 0,
+  },
+  {
+    id: "kdbadwak",
+    name: "Spawn 3",
+    x: 0,
+    y: 0,
+    z: 0,
+    pitch: 0,
+    yaw: 0,
+  },
+];
+
+export const columns: ColumnDef<Spawn>[] = [
+  {
+    accessorKey: "id",
+    header: "Id",
+  },
+  {
+    accessorKey: "name",
+    header: "Name",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const payment = row.original;
+
+      return (
+        <Button variant="destructive" className="h-8 w-8 p-0">
+          <HugeiconsIcon icon={Delete01Icon} size={20} />
+        </Button>
+      );
+    },
+  },
+];
+
+interface DataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[];
+  data: TData[];
+}
+
+export function DataTable<TData, TValue>({
+  columns,
+  data,
+}: DataTableProps<TData, TValue>) {
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+  });
+
+  return (
+    <>
+      <div className="overflow-hidden rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext(),
+                          )}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext(),
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <div className="flex items-center justify-end space-x-2 py-4">
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={() => table.previousPage()}
+          disabled={!table.getCanPreviousPage()}
+        >
+          Previous
+        </Button>
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={() => table.nextPage()}
+          disabled={!table.getCanNextPage()}
+        >
+          Next
+        </Button>
+      </div>
+    </>
+  );
+}
+
 function RouteComponent() {
-  return <div>Hello "/spawns"!</div>;
+  return (
+    <div className="container mx-auto p-10">
+      <DataTable columns={columns} data={spawns} />
+    </div>
+  );
 }
