ee2059374af98bab6e08d17ba8a0fe0598fd3af9
Joao Rocha <jrocha@silicolife.com>
2026-03-17 15:59:38 +0000

    Added GetMaps!

--- client/src/entities/map/map.ts
+++ client/src/entities/map/map.ts
@@ -1,4 +1,11 @@
+import { api } from "@/axios";
+
 export interface Map {
   id: string;
   name: string;
 }
+
+export const getMaps = async (): Promise<Map[]> => {
+  const { data } = await api.get("/maps");
+  return data;
+};
